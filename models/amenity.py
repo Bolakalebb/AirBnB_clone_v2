71b54c06c170d29f9b841b8fcd40c342bfdb90d1
Bolakale Aduloju <bolakalebb@gmail.com>
2023-03-23 16:01:03 -0700

    amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
-from models.amenity import Amenity
-import os
+""" State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from models import storage_type
+from sqlalchemy import Column, String
 
 
-class test_Amenity(test_basemodel):
-    """ amenity test class"""
-
-    def __init__(self, *args, **kwargs):
-        """inti the test class """
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
-
-    def test_name2(self):
-        """testing name type """
-        new = self.value()
-        self.assertEqual(type(new.name), str if
-                         os.getenv('HBNB_TYPE_STORAGE') != 'db' else
-                         type(None))
+class Amenity(BaseModel, Base):
+    '''amenity class'''
+    __tablename__ = 'amenities'
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+    else:
+        name = ""
