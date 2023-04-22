73881fd0eb8fae768c3e311710837b27dc1ab53e
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-04-22 18:19:50 +0100

    update compress

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,11 +5,11 @@ from datetime import date
 
 
 def do_pack():
-    """ A script that generates archive the contents of web_static folder"""
+    """Creates a compressed archive of the web_static folder."""
 
     filename = strftime("%Y%m%d%H%M%S")
     try:
-        local("mkdir -p versions")
+        local("sudo mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
               .format(filename))
 
