c6306fbf31b88e923bfac9fb98653dbdd4930e78
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-04-24 21:07:38 +0100

    update 2-c_route

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-“””Starts Flask web app“””
+"""starts a Flask web application
+web application must be listening on 0.0.0.0, port 5000
+"""
 
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 
 def hello():
-
     """Start Flask web application"""
-    
     return ('Hello HBNB!')
 
 @app.route('/hbnb', strict_slashes=False)
 
 def hbnb():
-
     """Adding route /hbnb"""
-
     return 'HBNB'
 
 @app.route('/c/<text>', strict_slashes=False)
 
 def cText(text):
-
     """Dynamic inputed text: replace _ for space and show text"""
-
     return 'C {}'.format(text.replace('_', ' '))
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=5000
