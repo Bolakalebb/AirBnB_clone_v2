1ad0d3e5505ae7458f88d6341d209bdce14e6620
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-04-24 21:09:14 +0100

    update 1-hbnb_route

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -5,22 +5,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 
 
 def hello():
     """Start Flask web application"""
-
     return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 
-
 def hbnb():
     """Adding route /hbnb"""
-
     return 'HBNB'
 
 if __name__ == '__main__':
-
     app.run(host='0.0.0.0', port=5000)
