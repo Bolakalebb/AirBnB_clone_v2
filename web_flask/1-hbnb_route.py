c7b9ceb7106cebdd9182f9145201bfea7126a229
Bolakale Aduloju <110998249+Bolakalebb@users.noreply.github.com>
2023-04-24 05:11:53 +0100

    Update

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
-"""starts a Flask web application
-web application must be listening on 0.0.0.0, port 5000
-"""
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
 
 from flask import Flask
 
@@ -10,15 +9,19 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 
 def hello():
-    """prints Hello HBNB"""
-    return "Hello HBNB!"
+
+    """Start Flask web application"""
+
+    return 'Hello HBNB!'
 
 @app.route('/hbnb', strict_slashes=False)
 
 def hbnb():
-    """add a specific route /hbnb"""
-    return "HBNB"
 
-if __name__ == "__main__":
+    """Adding route /hbnb"""
+
+    return 'HBNB'
+
+if __name__ == '__main__':
 
     app.run(host='0.0.0.0', port=5000)
